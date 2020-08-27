3b02d92d994cba3940d9d290f85453518b33550b
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-08-27 12:10:25 +1000

    Created Planet-List component

---
+++ planets-app/src/app/planets-list/planets-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { PlanetService } from '../planet.service'
+
+@Component({
+  selector: 'app-planets-list',
+  templateUrl: './planets-list.component.html',
+  styleUrls: ['./planets-list.component.css']
+})
+export class PlanetsListComponent implements OnInit {
+
+  constructor(public PlanetService: PlanetService) { }
+
+  ngOnInit(): void {
+  }
+
+}
