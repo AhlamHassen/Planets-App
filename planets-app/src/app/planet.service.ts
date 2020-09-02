d54cf717e6adf2a69fbe5e9f9ebd8fe9ebd562cd
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-09-03 00:07:49 +1000

    Updated planet services and planet class

--- planets-app/src/app/planet.service.ts
+++ planets-app/src/app/planet.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Planet } from './planet';
+import { Planet } from './planet' ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanetService {
-  
+
    Planets : Planet[] = [
-      new Planet ("Earth", 1, 30),
-      new Planet ("Mercury", 0, 10)
+      new Planet ("Mercury", 0, 120, "../../assets/mercury.png"),
+      new Planet ("Earth", 1, 300, "../../assets/earth.png"),
+      new Planet ("Mars", 0, 500, "../../assets/mars.png"),
+      new Planet ("Jupiter", 0, 620, "../../assets/jupiter.png")
    ];
 
-  constructor() { }
+  constructor() {}
+  
 
    public addPlanet(planet : Planet){
       this.Planets.push(planet);
    }
 
    public sortPlanet(){
-
+    // this.Planets.sort.
    }
 }
--- planets-app/src/app/planet.ts
+++ planets-app/src/app/planet.ts
@@ -2,11 +2,13 @@ export class Planet {
     Name: string;
     Moons: number;
     DistanceFromTheSun: number;
+    Image: string;
 
-    constructor(name:string, moons:number, distance:number){
+    constructor(name:string, moons:number, distance:number, url:string){
         this.Name = name;
         this.Moons = moons;
         this.DistanceFromTheSun = distance;
+        this.Image = url;
     }
 }
 
